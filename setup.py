e34c0e18ffbb84d567fe8c410c4ecb738c39d1df
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2017-09-21 16:50:31 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='optimatic',
+    packages=['optimatic'],
+    version='0.0.1',
+    description='Implementations of various optimisation algorithms',
+    author='Lewis O\'Driscoll',
+    author_email='lewis.odriscoll@gmail.com',
+    url='https://github.com/loddy1234/optimatic',
+    keywords=['optimisation', 'math'],
+    classifiers=[]
+)
